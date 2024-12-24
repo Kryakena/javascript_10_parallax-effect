88d8f3f542de139b5875663bb85706bc09c7d594
SvetlanaSher <svetl5he@yandex.ru>
2024-12-24 19:24:56 +0300

    Fix project

--- script.js
+++ script.js
@@ -8,13 +8,20 @@ $(document).ready(function () {
         var p_b = s/h_b*100; ////p_b - вычислим % (прокрутка только верхней части)
         var o = 1-1/100*p_b; //o - вычисление будущей непрозрачности
 
-        var z_1=1+(w/10000*p_b); //z_1 - прокрутка снега вместе с текстом
-      $('.parallax_snow').css('transform','scale('+z_1+')'); //непрозрачность снега
-      $('.parallax_snow').css('opacity',o); //значение o (английская буква)
+        var z_1=1+(w/10000*p_b); //z_1 - прокрутка тумана вместе с текстом
+      $('.parallax_fog').css('transform','scale('+z_1+')'); //непрозрачность тумана
+      $('.parallax_fog').css('opacity',o); //значение o (английская буква)
 
         var z_2=1+(w/5000000*p); //прокрутка всей контентной части
       $('.parallax_mountain_1').css('transform','scale('+z_2+')');
       //гору на заднем плане увеличить чуть-чуть при прокрутке контента
 
+        var hr=w/2000*p_b; //hr - горизонталь
+        var z_3=1+(w*0.000005*p_b);
+      $('.parallax_mountain_2').css('transform','translate3d('+hr+'px,0,0)','scale('+z_3+')');
+
+        var hr_2=w/1500*p_b; //hr - горизонталь
+        var z_4=1+(w*0.00001*p_b);
+      $('.parallax_mountain_3').css('transform','translate3d('+hr_2+'px,0,0)','scale('+z_4+')');
     });
 });
